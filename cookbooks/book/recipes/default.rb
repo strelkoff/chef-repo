d9e065095c4ee6f1f41667156b3a3f667178622c
strelkoff <strelkoff@bk.ru>
2016-08-20 00:30:36 +0700

    second commit

--- cookbooks/book/recipes/default.rb
+++ cookbooks/book/recipes/default.rb
@@ -7,6 +7,10 @@ package 'man' do
   action :install
 end
 
+package 'elrepo-release' do
+  action :install
+end
+
 cookbook_file "/root/test.sh" do
   source "test.sh"
   mode "0644"
--- cookbooks/yum-elrepo/attributes/testing.rb
+++
@@ -1,5 +0,0 @@
-
-default['yum']['elrepo-testing']['mirrorlist'] = "http://elrepo.org/mirrors-elrepo-testing.el#{node['platform_version'].to_i}"
-default['yum']['elrepo-testing']['description'] = 'ELRepo.org Testing Yum Repository'
-default['yum']['elrepo-testing']['gpgkey'] = 'http://elrepo.org/RPM-GPG-KEY-elrepo.org'
-default['yum']['elrepo-testing']['enabled'] = true
